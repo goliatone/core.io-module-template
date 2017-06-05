48412907ba32978242dae1a2e4fee03dd70e6092
goliatone <burgosemi@gmail.com>
2017-06-05 16:06:57 -0400

    Add prompt

---
+++ prompt.js
@@ -0,0 +1,32 @@
+'use strict';
+const slug = require('slug');
+slug.defaults.mode ='rfc3986';
+slug.defaults.modes['rfc3986'] = {
+    replacement: '-',      // replace spaces with replacement
+    symbols: true,         // replace unicode symbols or not
+    remove: null,          // (optional) regex to remove characters
+    lower: true,           // result in lower case
+    charmap: slug.charmap, // replace special characters
+    multicharmap: slug.multicharmap // replace multi-characters
+};
+
+let questions = [
+    {
+        type: 'input',
+        name: 'name',
+        default: function(answer) {
+            return answer.target;
+        },
+        message: 'What\'s the project\'s name?'
+    },
+    {
+        type: 'input',
+        name: 'moduleName',
+        default: function(answer) {
+            return slug(answer.name);
+        },
+        message: 'What\'s the module\'s name?'
+    }
+];
+
+module.exports = questions;
