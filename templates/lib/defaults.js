a73a91145c5c04e85a3e48f57f5e8b694ccc6987
goliatone <burgosemi@gmail.com>
2018-12-20 19:38:02 +0100

    update templates

--- templates/lib/defaults.js
+++ templates/lib/defaults.js
@@ -2,18 +2,5 @@
 'use strict';
 
 module.exports = {
-    orm: {
-        adapters: {
-            'disk':    require('sails-disk')
-        },
-        connections: {
-            development: {
-                adapter: 'disk'
-            }
-        },
-        defaults: {
-            migrate: process.env.NODE_ENV === 'production' ? 'safe' : 'drop',
-            connection: process.env.NODE_PERSISTENCE_CONNECTION || 'production'
-        }
-    }
+
 };
